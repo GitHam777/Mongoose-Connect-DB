299636a56d84b5f77ad3512523ad653092f7c149
arnav <arnabdas2753@gmail.com>
2024-03-28 23:04:23 +0530

    connection Isuue solved

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
-import express from "express"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app = express()
-dotenv.config()
-
-const PORT = process.env.PORT || 7000
-const MONGOURL = process.env.MONGO_URL
-
+const PORT = process.env.PORT || 3001;
 mongoose
-    .connect(MONGOURL)
-    .then(() => {
-        console.log("Database is Connected")
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`)
-        })
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://127.0.0.1:27017/test');
+const app = express();
 
-// const Cat = mongoose.model('Cat', { name: String });
-
-// const kitty = new Cat({ name: 'Zildjian' });
-// kitty.save().then(() => console.log('meow'));
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`); 
+});
